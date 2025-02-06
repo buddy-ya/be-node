49681936e1aa09b640aeebe2776c1cda6ce66838
mangsuyo <mangsuyo3610@gmail.com>
2025-02-06 15:42:05 +0900

    chore: gitignore 추가

--- config/db.js
+++ config/db.js
@@ -1,20 +1,26 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'local'}` });
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: '3.39.240.61',         // ✅ AWS EC2의 MySQL 호스트 IP (Spring과 동일)
-    user: 'root',                // ✅ MySQL 사용자 이름
-    password: 'Buddyya1234!',    // ✅ MySQL 비밀번호
-    database: 'buddy_ya',        // ✅ 데이터베이스 이름
-    port: 3306,                  // ✅ MySQL 기본 포트
-    ssl: { rejectUnauthorized: false } // 🔒 AWS 환경일 경우 SSL 설정 필요 시 사용
-});
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+if (process.env.DB_SSL === 'true') {
+  config.ssl = { rejectUnauthorized: false };
+}
+
+const connection = mysql.createConnection(config);
 
 connection.connect((err) => {
-    if (err) {
-        console.error('❌ MySQL 연결 실패:', err);
-        return;
-    }
-    console.log('✅ MySQL 연결 성공!');
+  if (err) {
+    console.error('❌ MySQL 연결 실패:', err.message);
+  } else {
+    console.log(`✅ MySQL 연결 성공`);
+  }
 });
 
 module.exports = connection;
