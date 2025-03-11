babae5a660349829ef8393f2613635ee220953ec
mangsuyo <mangsuyo3610@gmail.com>
2025-03-11 20:17:08 +0900

    feat: 디버깅 로그 추가 (#2)

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,8 +1,8 @@
 // routes/chatRoute.js
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const ChatController = require('../controller/ChatController');
+const ChatController = require("../controller/ChatController");
 
 // Multer 미들웨어: 메모리 스토리지를 사용해 파일을 버퍼 형태로 저장
 const upload = multer({ storage: multer.memoryStorage() });
@@ -12,5 +12,12 @@ const upload = multer({ storage: multer.memoryStorage() });
  * URL 예시: POST /node/chat/:roomId/image
  * 클라이언트는 FormData로 "image" 필드에 파일과 "tempId" 등을 전송합니다.
  */
-router.post('/:roomId/image', upload.single('image'), ChatController.uploadImages);
+
+console.log("채팅방 이미지 호출!");
+
+router.post(
+  "/:roomId/image",
+  upload.single("image"),
+  ChatController.uploadImages
+);
 module.exports = router;
