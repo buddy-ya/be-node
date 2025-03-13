9a66497461f000ea2f04515a36f3bd6d965a5435
mangsuyo <mangsuyo3610@gmail.com>
2025-03-13 13:30:06 +0900

    feat: 채팅방 이미지 전송 엔드포인트 변경 (#2)

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const httpAuthInterceptor = require('./middleware/HttpAuthInterceptor');
-const dotenv = require('dotenv');
+const httpAuthInterceptor = require("./middleware/HttpAuthInterceptor");
+const dotenv = require("dotenv");
 dotenv.config();
 
 app.use(express.json());
 
-const baseRoute = require('./routes/baseRoute');
-app.use('/node', baseRoute);
+const baseRoute = require("./routes/baseRoute");
+app.use("/node", baseRoute);
 
-const studentRoute = require('./routes/studentRoute');
-app.use('/node/students', studentRoute);
+const studentRoute = require("./routes/studentRoute");
+app.use("/node/students", studentRoute);
 
-const chatRoute = require('./routes/chatRoute');
-app.use('/node/rooms', httpAuthInterceptor, chatRoute);
+const chatRoute = require("./routes/chatRoute");
+app.use("/node/room", httpAuthInterceptor, chatRoute);
 
 // 전역 에러 핸들러
 app.use((err, req, res, next) => {
   console.error(err);
-  if (err instanceof require('./common/exception/BaseError')) {
+  if (err instanceof require("./common/exception/BaseError")) {
     return res.status(err.status).json({
       errorCode: err.errorCode,
       message: err.message,
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
   }
   res.status(500).json({
     errorCode: 0,
-    message: 'Internal Server Error',
+    message: "Internal Server Error",
   });
 });
 
